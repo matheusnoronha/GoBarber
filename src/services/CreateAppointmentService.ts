7336fddb77a7bcaba9f6c7fb22f07a97720a4718
matheus <noronhamatheus07@gmail.com>
2020-04-20 20:59:22 -0300

    init

---
+++ src/services/CreateAppointmentService.ts
@@ -0,0 +1,44 @@
+import { startOfHour } from 'date-fns';
+import Appointment from '../models/Appointment';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+
+interface RequestDTO {
+  provider: string;
+  date: Date;
+}
+
+/**
+ * Dependency Inversion (SOLI'D') (sempre que o service tiver uma dependencia externa
+ * no lugar de instanciar uma nova classe de repositorio, iremos receber ela
+ * como parametro do construtor, para que todos usem o mesmo repositorio);
+ * )
+ */
+
+class CreateAppointmentService {
+  private appointmentsRepository: AppointmentsRepository;
+
+  constructor(appointmentsRepository: AppointmentsRepository) {
+    this.appointmentsRepository = appointmentsRepository;
+  }
+
+  public execute({ date, provider }: RequestDTO): Appointment {
+    const appointmentDate = startOfHour(date);
+
+    const findAppointmentInSameDate = this.appointmentsRepository.findByDate(
+      date,
+    );
+
+    if (findAppointmentInSameDate) {
+      throw Error('This appointment is already booked');
+    }
+
+    const appointment = this.appointmentsRepository.create({
+      provider,
+      date: appointmentDate,
+    });
+
+    return appointment;
+  }
+}
+
+export default CreateAppointmentService;
