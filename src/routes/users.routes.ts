4bfc08fbcf5a4eb69d645f4deadda3860cc902d5
matheus <noronhamatheus07@gmail.com>
2020-04-26 13:57:12 -0300

    handling errors

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -34,17 +34,13 @@ usersRouter.patch(
   ensuraAuthenticated,
   upload.single('avatar'),
   async (request, response) => {
-    try {
-      const updateUserAvatar = new UpdateUserAvatarService();
-      const user = await updateUserAvatar.execute({
-        user_id: request.user.id,
-        avatarFilename: request.file.filename,
-      });
+    const updateUserAvatar = new UpdateUserAvatarService();
+    const user = await updateUserAvatar.execute({
+      user_id: request.user.id,
+      avatarFilename: request.file.filename,
+    });
 
-      return response.json(user);
-    } catch (err) {
-      return response.status(400).json({ error: err.message });
-    }
+    return response.json(user);
   },
 );
 
